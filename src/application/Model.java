3700c57c326c61beb29e5330f40111ffd423a9ca
Vitor Marques de Castro <vitormcastro@outlook.com>
2018-06-03 14:46:02 -0300

    Tela de login e classe model

---
+++ src/application/Model.java
@@ -0,0 +1,13 @@
+package application;
+
+public  class  Model {
+
+	public static class LoginModel{
+		
+		public static Boolean ValidateLogin(String nome, String senha) {
+			//ToDO --> conectar com a classe DAO do lucas e verificar no banco
+			//se está correto o login e senha
+			return true;
+		}
+	}
+}
