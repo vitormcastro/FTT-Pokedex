36e9748c9a0e0962b7d0284460f33e77696c5f3d
Vitor Marques de Castro <vitormcastro@outlook.com>
2018-06-03 23:16:24 -0300

    ok2

--- src/application/Model.java
+++ src/application/Model.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.util.ArrayList;
+
 public  class  Model {
 
 	public static class LoginModel{
 		
-		public static Boolean ValidateLogin(String nome, String senha) {
+		public static TreinadorVO ValidateLogin(String nome, String senha) {
 			//ToDO --> conectar com a classe DAO do lucas e verificar no banco
 			//se está correto o login e senha
-			return true;
+			return new TreinadorVO();
+		}
+	}
+	
+	public static class PokedexModel{
+		
+		public static ArrayList<PokemonVO> GetPokemon(int pag, int qtdd) {
+			//ToDO --> conectar com a classe DAO do lucas e trazer qtdd de pokemons igual no parametro
+			return new ArrayList<PokemonVO>();
 		}
 	}
 }
