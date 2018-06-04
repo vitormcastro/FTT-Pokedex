789a4593c4ab32049a9b4739351b8e8843a22bcf
Vitor Marques de Castro <vitormcastro@outlook.com>
2018-06-03 23:10:57 -0300

    ok

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,61 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	
+	
+	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+		
+		/*try {
+			FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(""));
+            AnchorPane root = (AnchorPane) loader.load();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		
 		launch(args);
 	}
+	
+	public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+        	FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("bin/Login"));
+            AnchorPane root = (AnchorPane) loader.load();
+
+            // Mostra a scene (cena) contendo o root layout.
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
 }
